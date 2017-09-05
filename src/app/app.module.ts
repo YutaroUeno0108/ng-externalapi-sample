4d3e2dadaabd451cca9c639462e0c3ccc57c1373
YutaroUeno0108 <yutaroueno@uenoyuutarou-no-MacBook-Air.local>
2017-09-06 03:25:07 +0900

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { JsonpModule} from "@angular/http";
+import { HttpService} from "./http.service";
+import {Ng2BootstrapModule} from "ng2-bootstrap/ng2-bootstrap";
+import {DetailComponent} from "./detail.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    JsonpModule,
+    Ng2BootstrapModule
+  ],
+  providers: [HttpService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
